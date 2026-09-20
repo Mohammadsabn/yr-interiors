3efecf1aba393b6dd4cd238231780248c70be990
Mohmmad <mohammedsabn@gmail.com>
2026-09-20 22:19:39 +0530

    add the user design section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { Hero } from "@/components/Hero";
 import { About } from "@/components/About";
 import { Services } from "@/components/Services";
 import { FeaturedProjects } from "@/components/FeaturedProjects";
+import { DesignYourSpace } from "@/components/DesignYourSpace";
 
 export default function Home() {
   return (
@@ -29,6 +30,7 @@ export default function Home() {
       <About />
       <Services />
       <FeaturedProjects />
+      <DesignYourSpace />
     </main>
   );
 }
